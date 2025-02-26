2bb73d2942bab0d7654cc30ccf116884815b6631
xiaohuioO <2729205098@qq.com>
2025-02-27 00:17:51 +0800

    2025-2-26

---
+++ 2025/2/2025-2-25/列表推导式筛选出'F'开头的单词.py
@@ -0,0 +1,3 @@
+word = ["Great","Fishc","Braiis","Exfda","Fantastic"]
+word1 = [i for i in word if i[0]=="F"]
+print(word1)
---
+++ 2025/2/2025-2-25/列表推导式获取矩阵副对角线上的元素.py
@@ -0,0 +1,6 @@
+matrix = [[1,2,3,4],
+          [5,6,7,8],
+          [9,10,11,12],
+          [13,14,15,16]]
+ls = [matrix[i][-i-1] for i in range(len(matrix))]
+print(ls)
